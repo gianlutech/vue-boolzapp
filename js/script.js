0b6c123834f7ded397fdf928f98982ded7ad509a
gianlutech <gianluca.ingrao95@gmail.com>
2022-02-01 16:17:46 +0100

    vue contact

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 console.log('JS OK');
+console.log('Vue ok', Vue);
+
+const root = new Vue({
+    el: '#root',
+    data: {
+        user: {
+            name: 'Barbara',
+            avatar: '_io'
+        },
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: '_1',
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+            },
+        ]
+    }
+});
